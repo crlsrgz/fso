948c2ca00439ca5c1d32f4b84c13447f3351af55
crlsrgz <course@carlosfx.com>
2024-10-27 20:50:48 +0100

    part1-exercise-1.1

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,21 @@
+import Content from "./components/Content.component";
+import Header from "./components/Header.component";
+import Total from "./components/Total.component";
+
+function App() {
+  const course = "Half Stack application development";
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14 
+
+  return (<div>
+    <Header title={course} />
+    <Content part1={part1} part2={part2} part3={part3}/>
+    <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+  </div>)
+}
+
+export default App
