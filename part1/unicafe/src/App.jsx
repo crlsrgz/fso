b890d23db6fc55fef4fe8d47f5e1200b12a120f2
crlsrgz <course@carlosfx.com>
2024-10-29 21:34:57 +0100

    add a total field

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
-
+import { useState } from "react";
 
 const App = () => {
- const [clicks, setClicks] = useState({
-  left: 0,
-  right: 0
- })
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
 
-const handleLeftClick = () => {
-  setClicks({...clicks, left:clicks.left + 1})
-}
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
 
-const handleRightClick = () => {
-  setClicks({...clicks, right: clicks.right + 1})
-}
+  /**
+   * handleLeftClick
+   * set the clicks spreading the object values,then modifies the the click value
+   * Instead of using push, that mutates the array the concat method returns a new array
+   * updateLeft creates a copy and then is used to set the left and set the total
+   */
+  const handleLeftClick = () => {
+    const updateLeft = clicks.left + 1;
+    setAll(allClicks.concat("L"));
+    setClicks({ ...clicks, left: updateLeft });
+    setTotal(updateLeft + clicks.right);
+  };
+
+  const handleRightClick = () => {
+    const updateRight = clicks.right + 1;
+    setAll(allClicks.concat("R"));
+    setClicks({ ...clicks, right: updateRight });
+    setTotal(clicks.left + updateRight);
+  };
 
   return (
     <div>
@@ -21,8 +35,10 @@ const handleRightClick = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {clicks.right}
+      <p>{allClicks.join(" ")}</p>
+      <p>Total {total}</p>
     </div>
-  )
-}
+  );
+};
 
 export default App;
