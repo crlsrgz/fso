02a808d1a32fce971730aa2ae1c8f78e98c65baf
crlsrgz <course@carlosfx.com>
2024-10-29 13:27:46 +0100

    part1d-exercise-1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,13 +6,37 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  function Button({onSmash, text}){
+    console.log("click")
+    return(
+      <button onClick={onSmash}>{text}</button>
+    )
+  }
+  
+  const increaseCount = () => setGood(good + 1)
+  const increaseNeutral = () => setNeutral(neutral + 1)
+  const increaseBad = () => setBad(bad + 1)
+
   return (
     <div>
-      <p>{good}</p>
-      <p>{neutral}</p>
-      <p>{bad}</p>
+      <header>
+        <h2>Give feedback</h2>
+      </header>
+      <div>
+        <Button onSmash={increaseCount} text={"good"} />
+        <Button onSmash={increaseNeutral} text={"neutral"} />
+        <Button onSmash={increaseBad} text={"bad"} />
+
+      </div>
+
+      <div>
+        <h2>statistics</h2>
+        <p><span>good </span><span>{good}</span></p>
+        <p><span>netral </span><span>{neutral}</span></p>
+        <p><span>bad </span><span>{bad}</span></p>
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
