7948d789b2ee27f626708ae6559009bcd546d0a5
crlsrgz <course@carlosfx.com>
2024-10-29 15:32:46 +0100

    WIP part1d-exercise-1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,21 +1,37 @@
 import { useState } from 'react'
 
+function Button({onSmash, text}){
+  return(
+    <button onClick={onSmash}>{text}</button>
+  )
+}
+function calculateValues(good, neutral, bad){
+  console.log(good, neutral, bad)
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  function Button({onSmash, text}){
-    console.log("click")
-    return(
-      <button onClick={onSmash}>{text}</button>
-    )
-  }
+  const [allPoints, setAllPoints] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
   
-  const increaseCount = () => setGood(good + 1)
-  const increaseNeutral = () => setNeutral(neutral + 1)
-  const increaseBad = () => setBad(bad + 1)
+  const increaseCount = () => {
+    setGood(good + 1) 
+    calculateValues(good, neutral, bad)
+  }
+  const increaseNeutral = () => {
+    setNeutral(neutral + 1)
+    calculateValues(good, neutral, bad)
+  } 
+  const increaseBad = () => {
+    setBad(bad + 1)
+    calculateValues(good, neutral, bad)   
+  }
 
   return (
     <div>
@@ -34,6 +50,9 @@ const App = () => {
         <p><span>good </span><span>{good}</span></p>
         <p><span>netral </span><span>{neutral}</span></p>
         <p><span>bad </span><span>{bad}</span></p>
+        <p><span>all </span><span>{allPoints}</span></p>
+        <p><span>average </span><span>{average}</span></p>
+        <p><span>positive </span><span>{positive} %</span></p>
       </div>
     </div>
   )
