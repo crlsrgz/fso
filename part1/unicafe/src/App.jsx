9cb669439c5cea6c0dcb8c98d72a341ff2328788
crlsrgz <course@carlosfx.com>
2024-10-30 13:24:54 +0100

    part1d-exercise-1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,37 +1,47 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-function Button({onSmash, text}){
-  return(
-    <button onClick={onSmash}>{text}</button>
-  )
-}
-function calculateValues(good, neutral, bad){
-  console.log(good, neutral, bad)
+function Button({ onSmash, text }) {
+  return <button onClick={onSmash}>{text}</button>;
 }
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const [allPoints, setAllPoints] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
+  const [allPoints, setAllPoints] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
-  
+  function calculateValues(good, neutral, bad) {
+    const calcAll = good + neutral + bad;
+    const calcAverage = (good - bad) / calcAll;
+    const calcPositive = (good / calcAll) * 100;
+
+    console.log(good, neutral, bad);
+    console.log(calcAll, calcAverage, calcPositive);
+
+    setAllPoints(calcAll);
+    setAverage(calcAverage);
+    setPositive(calcPositive);
+  }
+
   const increaseCount = () => {
-    setGood(good + 1) 
-    calculateValues(good, neutral, bad)
-  }
+    const incrementeGood = good + 1;
+    setGood(incrementeGood);
+    calculateValues(incrementeGood, neutral, bad);
+  };
   const increaseNeutral = () => {
-    setNeutral(neutral + 1)
-    calculateValues(good, neutral, bad)
-  } 
+    const incrementNeutral = neutral + 1;
+    setNeutral(incrementNeutral);
+    calculateValues(good, incrementNeutral, bad);
+  };
   const increaseBad = () => {
-    setBad(bad + 1)
-    calculateValues(good, neutral, bad)   
-  }
+    const incrementBad = bad + 1;
+    setBad(incrementBad);
+    calculateValues(good, neutral, incrementBad);
+  };
 
   return (
     <div>
@@ -42,20 +52,37 @@ const App = () => {
         <Button onSmash={increaseCount} text={"good"} />
         <Button onSmash={increaseNeutral} text={"neutral"} />
         <Button onSmash={increaseBad} text={"bad"} />
-
       </div>
 
       <div>
         <h2>statistics</h2>
-        <p><span>good </span><span>{good}</span></p>
-        <p><span>netral </span><span>{neutral}</span></p>
-        <p><span>bad </span><span>{bad}</span></p>
-        <p><span>all </span><span>{allPoints}</span></p>
-        <p><span>average </span><span>{average}</span></p>
-        <p><span>positive </span><span>{positive} %</span></p>
+        <p>
+          <span>good </span>
+          <span>{good}</span>
+        </p>
+        <p>
+          <span>neutral </span>
+          <span>{neutral}</span>
+        </p>
+        <p>
+          <span>bad </span>
+          <span>{bad}</span>
+        </p>
+        <p>
+          <span>all </span>
+          <span>{allPoints}</span>
+        </p>
+        <p>
+          <span>average </span>
+          <span>{average}</span>
+        </p>
+        <p>
+          <span>positive </span>
+          <span>{positive} %</span>
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
