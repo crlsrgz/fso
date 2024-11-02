14f65a420232b95494abbaeb0039edf555c4b69e
crlsrgz <course@carlosfx.com>
2024-11-02 18:38:57 +0100

    part2: setup

--- part2/collections/src/App.jsx
+++ part2/collections/src/App.jsx
@@ -1,7 +1,17 @@
-import viteLogo from "/vite.svg";
-
-function App() {
-  return <></>;
+function App(props) {
+  const { notes } = props;
+  return (
+    <>
+      <div>
+        <h1>Notes</h1>
+        <ul>
+          <li>{notes[0].content}</li>
+          <li>{notes[1].content}</li>
+          <li>{notes[2].content}</li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
