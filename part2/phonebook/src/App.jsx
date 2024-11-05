198752c95e23b27c887f5e770621944b2c7b9e20
crlsrgz <course@carlosfx.com>
2024-11-05 05:28:11 +0100

    Part2: Exercise 2.7 // WIP Phonebook app

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,26 +1,61 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function App() {
- const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addTelphoneNumber = (event) => {
+    event.preventDefault();
+
+    const newEntry = {
+      name: newName,
+    };
+
+    let newEntryAlreadyExists = false;
+    const arrayToCheck = [...persons];
+
+    arrayToCheck.forEach((item) => {
+      if (item.name === newEntry.name) {
+        console.log("already here");
+        newEntryAlreadyExists = true;
+      }
+    });
+    // console.log(newEntry.name);
+    // console.log(arrayToCheck.includes(newEntry));
+    // console.log(arrayToCheck);
+    if (newEntryAlreadyExists) {
+      setNewName("");
+
+      return;
+    }
+    setPersons(persons.concat(newEntry));
+    setNewName("");
+  };
+
+  // function checkIfEntryAlreadyExists(persons, newEntry) {}
+
+  const handleInputValue = (event) => {
+    // console.log(event.target.value);
+    setNewName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addTelphoneNumber}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputValue} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((item) => {
+        return <div key={item.name}>{item.name}</div>;
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
