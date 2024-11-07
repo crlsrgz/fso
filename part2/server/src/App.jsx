f08d6ff4257ecc2cb25a58409f27c4add83ebf65
crlsrgz <course@carlosfx.com>
2024-11-07 12:53:55 +0100

    part 2x: server useEffect

--- part2/server/src/App.jsx
+++ part2/server/src/App.jsx
@@ -1,41 +1,55 @@
 import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 function App() {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
   const promise = axios.get("http://localhost:3001/notes");
+  // //displays the promise status, in this instance "pending"
+  // console.log(promise);
 
-  //displays the promise status, in this instance "pending"
-  console.log(promise);
+  // promise.then((response) => {
+  //   // display the response content, Object config, data, headers and request
+  //   console.log(response);
+  //   // use data key to acces the filling
+  //   const notes = response.data;
+  //   console.log(notes);
+  // });
 
-  promise.then((response) => {
-    // display the response content, Object config, data, headers and request
-    console.log(response);
-    // use data key to acces the filling
-    const notes = response.data;
-    console.log(notes);
-  });
+  // // Another shorter version
+  // axios.get("http://localhost:3001/notes").then((response) => {
+  //   const notes = response.data;
+  //   console.log(notes);
+  // });
 
-  // Another shorter version
-  axios.get("http://localhost:3001/notes").then((response) => {
-    const notes = response.data;
-    console.log(notes);
-  });
+  // // assign values
+  // const renderRes = axios
+  //   .get("http://localhost:3001/notes")
+  //   .then((response) => {
+  //     const notes = response.data;
+  //     console.log(typeof notes);
+  //     console.log(notes[0]);
+  //     return notes;
+  //   });
 
-  // assign values
-  const renderRes = axios
-    .get("http://localhost:3001/notes")
-    .then((response) => {
-      const notes = response.data;
-      console.log(typeof notes);
-      console.log(notes[0]);
-      return notes;
+  // // const promise2 = axios.get('http://localhost:3001/foobar')
+  // // console.log(promise2)
+
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
     });
+  };
 
-  // const promise2 = axios.get('http://localhost:3001/foobar')
-  // console.log(promise2)
+  useEffect(hook, []);
 
   return (
     <>
       <div>Hello</div>
-      <div>{console.log("returned", renderRes)}</div>
+      {/* <div>{console.log("returned", renderRes)}</div> */}
       {/* {renderRes.map((item) => {
         return <p key={item.id}>{item.content}</p>;
       })} */}
