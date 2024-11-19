37f6daa835b13e1c371ded635f011faad38647f5
crlsrgz <course@carlosfx.com>
2024-11-19 05:38:15 +0100

    Part 3b: Lessons Dplying app start, copy of notes apss in part 3

---
+++ part3/notes/src/App.jsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+import Note from "./components/Note";
+import axios from "axios";
+import noteService from "./services/notes";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState(`a new note...`);
+  const [showAll, setShowAll] = useState(true);
+  const [errorMesage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+      // console.log(initialNotes);
+    });
+  }, []);
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changeNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changeNote)
+      .then((returnedNote) => {
+        setNotes(
+          notes.map((note) => {
+            return note.id === id ? returnedNote : note;
+          })
+        );
+      })
+      .catch((error) => {
+        // alert(`the note ${note.content} was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
+  };
+
+  /**
+   * addNote function
+   * 1. prevents the default event of the form
+   * 2. creates a new object using the newNote state.
+   * 3. important key is set randomly to a boolean
+   * 4. the the id key is set using the length of the array and adding  1
+   * @param {MyEvent} event - the event of the input
+   * @listens MyEvent
+   */
+  const addNote = (event) => {
+    event.preventDefault();
+    // console.log("button clicked", event.target);
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      // id: String(notes.length + 1),
+    };
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+    });
+    setNewNote("");
+  };
+
+  const handleNoteChange = (event) => {
+    // console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMesage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => {
+          return (
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={() => {
+                toggleImportanceOf(note.id);
+              }}
+            />
+          );
+        })}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ part3/notes/src/services/notes.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+const BASEURL = "http://localhost:3001/api/notes";
+
+const getAll = () => {
+  const request = axios.get(BASEURL);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+  const request = axios.post(BASEURL, newObject);
+  return request.then((response) => response.data);
+};
+
+const updateTest = (id, newObject) => {
+  return axios
+    .put(`${BASEURL}/${id}`, newObject)
+    .then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${BASEURL}/${id}`, newObject);
+  return request.then((res) => {
+    return res.data;
+  });
+};
+
+export default { getAll, create, update, updateTest };
