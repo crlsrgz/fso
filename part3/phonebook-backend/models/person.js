efdfd55cbe0a7e9e698668ae97e014df8a12d701
crlsrgz <course@carlosfx.com>
2024-11-21 21:07:17 +0100

    Part 3c: Exercise 3.13 - created module for contacts

---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const url = process.env.MONGODB_URI;
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(url)
+  .then((resulr) =>
+    console.log(`-------\nConnection to MDB established\n-------\n`)
+  )
+  .catch((error) => {
+    console.log("Connection error", error.message);
+  });
+
+const contactSchema = new mongoose.Schema({
+  name: String,
+  phoneNumber: String,
+});
+
+contactSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Contact", contactSchema);
