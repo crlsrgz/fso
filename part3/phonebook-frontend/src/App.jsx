ed5fd38b70c1a0f8ab11288f68b31148558a88d5
crlsrgz <course@carlosfx.com>
2024-11-27 09:14:11 +0100

    Part3d: Exercise 3.20 - Phonebook, fix: wrong class for added contact message

--- part3/phonebook-frontend/src/App.jsx
+++ part3/phonebook-frontend/src/App.jsx
@@ -24,7 +24,7 @@ function Messagefeedback(props) {
     class: (function () {
       switch (props.message) {
         case "add":
-          return `add`;
+          return `accepted`;
         case "info":
           return `error`;
         case "short":
