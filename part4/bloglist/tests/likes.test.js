e24029ce3e1462fab8d34094f4728e1dd3bdc79a
crlsrgz <course@carlosfx.com>
2024-12-07 06:07:42 +0100

    Part 4a: Exercise 4.4 - Bloglist: Testing for total likes

--- part4/bloglist/tests/likes.test.js
+++ part4/bloglist/tests/likes.test.js
@@ -3,17 +3,26 @@ const assert = require("node:assert");
 
 const blogEntries = require("../utils/list_helper_data");
 const totalLikes = require("../utils/list_helper").totalLikes;
-// test for a total, no entries and one entry
+
 describe("Likes Test", () => {
   test("Sum of likes", () => {
-    const blogs = blogEntries;
-    const result = totalLikes(blogEntries);
+    const blogs = blogEntries.blogEntries;
+    const result = totalLikes(blogs);
     assert.strictEqual(result, 36);
   });
 });
+
 describe("Likes Test", () => {
   test("First entry likes", () => {
-    const result = blogEntries[0].likes;
+    const result = blogEntries.oneBlogEntry[0].likes;
     assert.strictEqual(result, 7);
+  });
+});
+
+describe("Likes Test", () => {
+  test("Empty array", () => {
+    const blogs = [];
+    const result = totalLikes(blogs);
+    assert.strictEqual(result, "No Blog entries");
   });
 });
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -12,11 +12,18 @@ const dummy = (blogs) => {
 };
 
 const totalLikes = (array) => {
-  const total = array.reduce((acc, x) => {
-    // console.log("===>", x.likes);
-    return acc + x.likes;
-  }, 0);
-  return total;
+  if (array.length < 1) {
+    return "No Blog entries";
+  }
+  if (array.length === 1) {
+    return array[0].likes;
+  } else {
+    const total = array.reduce((acc, x) => {
+      // console.log("===>", x.likes);
+      return acc + x.likes;
+    }, 0);
+    return total;
+  }
 };
 
 module.exports = { dummy, totalLikes };
