60f41e014c4b032c7d85941b6c54d6a9a3094ac0
crlsrgz <course@carlosfx.com>
2024-12-08 21:14:58 +0100

    Part 4a: Exercise 4.8 - Bloglist: Tests, response and count of blog entries

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -1,13 +1,50 @@
-const { test, after, describe } = require("node:test");
+const { test, after, describe, beforeEach } = require("node:test");
 const assert = require("node:assert");
 
-describe.only("Initial", () => {
-  test.only("API test initial setup", () => {
+const helper = require("./test_helpers");
+
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+
+const app = require("../app");
+const api = supertest(app);
+
+const blogEntry = require("../models/blogEntry");
+
+// Clear DB and create entries for every test
+beforeEach(async () => {
+    await blogEntry.deleteMany({});
+    console.log("cleared");
+
+    for (let blog of helper.blogs) {
+        const blogObject = new blogEntry(blog);
+        await blogObject.save();
+        // console.log("saved", blogObject);
+    }
+
+    console.log("done");
+});
+
+test("API test initial setup", () => {
     const result = 111;
     assert.strictEqual(result, 111);
-  });
-  test.only("Another API test initial setup", () => {
-    const result = 111;
-    assert.strictEqual(result, 111);
-  });
+});
+
+test.only("Return JSON Content", async () => {
+    await api
+        .get("/api/blogs")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+});
+
+test.only("There are three blogs", async () => {
+    const response = await api.get("/api/blogs");
+    const responseLength = await response.body.length;
+    const initialLength = helper.blogs.length;
+
+    assert.strictEqual(responseLength, initialLength);
+});
+
+after(async () => {
+    await mongoose.connection.close();
 });
