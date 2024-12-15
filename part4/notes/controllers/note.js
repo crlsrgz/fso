e982a95d45693f0a2e666075b278c12565285b66
crlsrgz <course@carlosfx.com>
2024-12-15 18:00:46 +0100

    Part 4c: Lesson - Notes: populate notes/user with info from each other

--- part4/notes/controllers/note.js
+++ part4/notes/controllers/note.js
@@ -10,7 +10,9 @@ notesRouter.get("/", (request, response) => {
 });
 
 notesRouter.get("/api/notes", async (request, response) => {
-  const notes = await Note.find({});
+  // const notes = await Note.find({});
+  // const notes = await Note.find({}).populate("user");
+  const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
   response.json(notes);
 });
 
