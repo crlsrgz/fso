49111e03f7a932a65e9a9df4b8247f0f8f04ff08
crlsrgz <course@carlosfx.com>
2025-01-19 15:46:47 +0100

    Part5b: BloglistFE - EXE 5.7 Blog List Frontend, initial setup

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,26 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  console.log(blog);
+  return (
+    <div style={blogStyle}>
+      <div>{blog.title}</div>
+      <div>{blog.url}</div>
+      <div>
+        {' '}
+        <span>likes: {blog.likes}</span>
+        <span>
+          <button>like</button>
+        </span>{' '}
+      </div>
+      <div>{blog.author}</div>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
