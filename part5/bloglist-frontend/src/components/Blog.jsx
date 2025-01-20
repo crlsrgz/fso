c292c61b17817844ffde5d4f8d1572e04b13fed1
crlsrgz <course@carlosfx.com>
2025-01-20 14:38:09 +0100

    Part5b: BloglistFE - EXE 5.8 BloglistFE: Like functionality in fronend and backend

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
+import blogService from '../services/blogs';
+
 const Blog = ({ blog }) => {
+  const [likes, setLikes] = useState(blog.likes);
+
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
@@ -6,16 +11,34 @@ const Blog = ({ blog }) => {
     borderWidth: 1,
     marginBottom: 5,
   };
-  console.log(blog);
+
+  const handleLikeEntry = async (event) => {
+    event.preventDefault();
+
+    // const updateLikes = (await response.likes) + 1;
+    // console.log('Updated', updateLikes);
+    // setLikes(updateLikes);
+
+    setLikes(likes + 1);
+    try {
+      const request = await blogService.likeEntry(blog);
+      return request.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div style={blogStyle}>
       <div>{blog.title}</div>
       <div>{blog.url}</div>
       <div>
         {' '}
-        <span>likes: {blog.likes}</span>
+        <span>likes: {likes}</span>
         <span>
-          <button>like</button>
+          <form onSubmit={handleLikeEntry}>
+            <button>like</button>
+          </form>
         </span>{' '}
       </div>
       <div>{blog.author}</div>
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -19,4 +19,20 @@ const postEntry = async (blogEntry) => {
   const request = await axios.post(baseUrl, blogEntry, config);
 };
 
-export default { getAll, postEntry, setToken };
+const likeEntry = async (blogEntry) => {
+  // console.log(blogEntry);
+  const url = `${baseUrl}/${blogEntry.id}`;
+  const updatedLikes = blogEntry.likes + 1;
+  const updatedEntry = {
+    user: blogEntry.user,
+    likes: updatedLikes,
+    author: blogEntry.author,
+    title: blogEntry.title,
+    url: blogEntry.url,
+  };
+  const request = await axios.put(url, updatedEntry);
+  // console.log(request.data);
+  return request.data;
+};
+
+export default { getAll, postEntry, setToken, likeEntry };
