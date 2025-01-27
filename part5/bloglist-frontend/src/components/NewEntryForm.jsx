f86d8da5b28b84870bf7acab2ee59e3a5fab4281
crlsrgz <course@carlosfx.com>
2025-01-27 16:51:03 +0100

    Part5b: BloglistFE - EXE 5.12 - PropTypes

--- part5/bloglist-frontend/src/components/NewEntryForm.jsx
+++ part5/bloglist-frontend/src/components/NewEntryForm.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 const NewEntryForm = ({ createBlogEntry }) => {
@@ -60,5 +61,7 @@ const NewEntryForm = ({ createBlogEntry }) => {
     </>
   );
 };
-
+NewEntryForm.propTypes = {
+  createBlogEntry: PropTypes.func.isRequired,
+};
 export default NewEntryForm;
--- part5/bloglist-frontend/src/components/Togglable.jsx
+++ part5/bloglist-frontend/src/components/Togglable.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 const Togglable = (props) => {
@@ -24,4 +25,7 @@ const Togglable = (props) => {
   );
 };
 
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
 export default Togglable;
