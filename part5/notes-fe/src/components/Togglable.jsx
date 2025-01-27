874c05da6b8dae0f4e3c6cca7cdd66e9fc08cac7
crlsrgz <course@carlosfx.com>
2025-01-27 13:16:13 +0100

    Part 5b: Notes - PropTypes

--- part5/notes-fe/src/components/Togglable.jsx
+++ part5/notes-fe/src/components/Togglable.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { forwardRef, useImperativeHandle, useState } from "react";
 
 const Togglable = forwardRef((props, refs) => {
@@ -30,5 +31,9 @@ const Togglable = forwardRef((props, refs) => {
     </>
   );
 });
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
 
 export default Togglable;
