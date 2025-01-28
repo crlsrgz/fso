596ea34ce99fb76881fb28a02946aea321cf1e93
crlsrgz <course@carlosfx.com>
2025-01-28 14:27:28 +0100

    Part5c: Notes - Testing react apps

---
+++ part5/notes-fe/src/components/Note.test.jsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Note from "./Note";
+
+test("renders content", () => {
+  const note = {
+    content: "component testing is done with react-testing-library",
+    important: true,
+  };
+  render(<Note note={note} />);
+  const element = screen.getByText(
+    "component testing is done with react-testing-library"
+  );
+  expect(element).toBeDefined();
+});
