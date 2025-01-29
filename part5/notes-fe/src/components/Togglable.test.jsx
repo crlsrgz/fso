bfd0f70cefc17986bd5ad768d7ebd51c25b2baa8
crlsrgz <course@carlosfx.com>
2025-01-29 21:19:29 +0100

    Part5c: Notes - Testing buttons, comments

--- part5/notes-fe/src/components/Togglable.test.jsx
+++ part5/notes-fe/src/components/Togglable.test.jsx
@@ -6,7 +6,11 @@ import Togglable from "./Togglable";
 describe("<Togglable />", () => {
   let container;
 
+  /**
+   * Run the function before each test to render the component
+   */
   beforeEach(() => {
+    // Render the component
     container = render(
       <Togglable buttonLabel="show...">
         <div className="testDiv">togglable content</div>
@@ -15,14 +19,23 @@ describe("<Togglable />", () => {
   });
 
   test("renders its children", async () => {
+    /**
+     * wait for the elements to appear in the DOM,
+     */
     await screen.findAllByText("togglable content");
   });
 
   test("at start the children are not displayed", () => {
     const div = container.querySelector(".togglableContent");
+    /**
+     * Check if the queried div has the style display none
+     */
     expect(div).toHaveStyle("display:none;");
   });
   test("after clikcking the buttun, children are displayed", async () => {
+    /**
+     * Initialize a simulation, setup() returns an object used to call click(), type(), hover(), simulating user actions
+     */
     const user = userEvent.setup();
     const button = screen.getByText("show...");
     await user.click(button);
