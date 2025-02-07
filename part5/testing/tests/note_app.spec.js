da6ef3ecb59657c30d4c0e6a9cafac4587f43615
crlsrgz <course@carlosfx.com>
2025-02-07 11:35:45 +0100

    Part5d: Notes - Playright failed test, limit test time

--- part5/testing/tests/note_app.spec.js
+++ part5/testing/tests/note_app.spec.js
@@ -1,9 +1,14 @@
 const {test, expect} = require("@playwright/test")
+const { describe } = require("node:test")
 
-test("front page can be opened", async({page}) => {
-  await page.goto("http://localhost:5173")
+describe("Note app", () => {
 
-  const locator = await page.getByText("Notes")
-  await expect(locator).toBeVisible()
-  await expect(page.getByText("CSS is easy")).toBeVisible()
+  test("front page can be opened", async({page}) => {
+    await page.goto("http://localhost:5173")
+
+    const locator = await page.getByText("Notes")
+    await expect(locator).toBeVisible()
+    await expect(page.getByText("CSS is easy")).toBeVisible()
+    await expect(page.getByText("Single page app token based auth1")).toBeVisible()
+  })
 })
