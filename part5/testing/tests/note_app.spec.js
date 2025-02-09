34c3fe181f7eb3d2d5f2f5820ea050c91a8cfe29
crlsrgz <course@carlosfx.com>
2025-02-09 14:29:29 +0100

    Part5d: Notes - Playright test Form input, prepare frontend with test ids

--- part5/testing/tests/note_app.spec.js
+++ part5/testing/tests/note_app.spec.js
@@ -9,6 +9,20 @@ describe("Note app", () => {
     const locator = await page.getByText("Notes")
     await expect(locator).toBeVisible()
     await expect(page.getByText("CSS is easy")).toBeVisible()
-    await expect(page.getByText("Single page app token based auth1")).toBeVisible()
+    await expect(page.getByText("Single page app token based auth")).toBeVisible()
+  })
+
+  test("login form can be opened", async({page}) => {
+    await page.goto("http://localhost:5173")
+
+    await page.getByRole("button", {name:"Log in"}).click()
+
+    await page.getByRole("textbox").first().fill("Jon")
+    await page.getByRole("textbox").last().fill("hello")
+
+    await page.getByRole("button", {name: "log in"}).click()
+    
+    await expect(page.getByText("Jon Jones logged in")).toBeVisible()
   })
 })
+
