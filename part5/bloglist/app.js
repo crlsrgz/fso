da2f7de0d7b38cd3b8042cd8d1e6e1a0b78d161a
crlsrgz <course@carlosfx.com>
2025-02-13 20:57:31 +0100

    Part5d: Bloglist - 5.17: WIP - Blog List backend setup

--- part5/bloglist/app.js
+++ part5/bloglist/app.js
@@ -38,6 +38,11 @@ app.use("/", blogRouter);
 app.use("/", userRouter);
 app.use("/", loginRouter);
 
+if (process.env.NODE_ENV === "test") {
+    const testingRouter = require("./controllers/testing");
+    app.use("/api/testing", testingRouter);
+}
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
