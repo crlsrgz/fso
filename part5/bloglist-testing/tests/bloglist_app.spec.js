4f00e0a03dbd6d90572b3e7af91af83539792dba
crlsrgz <course@carlosfx.com>
2025-02-13 22:21:23 +0100

    Part5d: Bloglist - 5.17 - 5.18: WIP - Blog List testing login form

--- part5/bloglist-testing/tests/bloglist_app.spec.js
+++ part5/bloglist-testing/tests/bloglist_app.spec.js
@@ -4,8 +4,37 @@ describe("Blog app",() => {
   beforeEach(async ({page}) => {
     await page.goto("http://localhost:5173")
   })
-  test("Login form is shown", async({page})=>{
+  test("Homepage is loaded", async({page})=>{
     const locator = await page.getByText("blogs list")
     await expect(locator).toBeVisible()
+  })
+
+  describe("Form testing", () => {
+
+    test("Login form is shown", async({page}) => {
+
+      const loginButton = page.getByRole("button",  {name: "login"})
+      const usernameInput = page.getByTestId("username")
+      const passwordInput = page.getByTestId("password")
+
+      await expect(loginButton).toBeVisible()
+      await expect(usernameInput).toBeVisible()
+      await expect(passwordInput).toBeVisible()
+      
+    })
+
+    test("User can log in", async({page}) =>{
+      const loginButton = page.getByRole("button", {name: "login"})
+      const usernameInput = page.getByTestId("username")
+      const passwordInput = page.getByTestId("password")
+
+      await usernameInput.fill("legolas")
+      await  passwordInput.fill("ethelum")
+      await loginButton.click()
+
+      await(expect(page.getByText("logged in as legolas"))).toBeVisible()
+      
+    })
+
   })
 })
