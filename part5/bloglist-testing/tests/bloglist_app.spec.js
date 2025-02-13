ec105a6edf20f29749c2fc1ab412a88fef4b8e6f
crlsrgz <course@carlosfx.com>
2025-02-13 14:56:19 +0100

    Part5d: Bloglist - 5.17: WIP - Blog List End To End Testing, step 1

---
+++ part5/bloglist-testing/tests/bloglist_app.spec.js
@@ -0,0 +1,11 @@
+import { test, expect,describe, beforeEach } from '@playwright/test';
+
+describe("Blog app",() => {
+  beforeEach(async ({page}) => {
+    await page.goto("http://localhost:5173")
+  })
+  test("Login form is shown", async({page})=>{
+    const locator = await page.getByText("blogs list")
+    await expect(locator).toBeVisible()
+  })
+})
