27fc5c3f20ecce35ea492427259d0e1c352ecab7
crlsrgz <course@carlosfx.com>
2025-02-18 21:31:07 +0100

    Part 5d: Bloglist - 5.20 List End To End Testing, step 4

--- part5/bloglist-testing/tests/helper.js
+++ part5/bloglist-testing/tests/helper.js
@@ -10,4 +10,22 @@ const loginWith = async (page, username, password) => {
       await loginButton.click()
 }
 
-export {loginWith}
+const createDummyEntry = async (page) => {
+
+      const addBlogButton = page.getByRole("button", {name: "Add new Blog"})
+      await addBlogButton.click()
+      
+      const addNewButton = page.getByRole("button", {name:"Add new"})
+      const inputBlogTitle = page.getByTestId("blogTitle")
+      const inputBlogAuthor = page.getByTestId("blogAuthor")
+      const inputBlogUrl = page.getByTestId("blogUrl")
+
+
+      await inputBlogTitle.fill("The Hobbit")
+      await inputBlogAuthor.fill("Bilbo")
+      await inputBlogUrl.fill("theshire.com")
+
+      await addNewButton.click()
+}
+
+export {loginWith, createDummyEntry}
