dafd20e9f091844dd05c199ee6beaadfcc9e7039
crlsrgz <course@carlosfx.com>
2025-02-19 20:51:22 +0100

    Part 5d: chore - prettier format

--- part5/bloglist-testing/tests/bloglist_app.spec.js
+++ part5/bloglist-testing/tests/bloglist_app.spec.js
@@ -1,51 +1,46 @@
-import { test, expect,describe, beforeEach } from '@playwright/test';
-import { createDummyEntry, logOutFrom, loginWith } from './helper';
+import { test, expect, describe, beforeEach } from "@playwright/test";
+import { createDummyEntry, logOutFrom, loginWith } from "./helper";
 
-describe("Blog app",() => {
-
-  beforeEach(async ({page, request}) => {
-    await request.post("api/testing/reset")
+describe("Blog app", () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post("api/testing/reset");
 
     await request.post("api/users", {
-      data:{
+      data: {
         name: "Aragorn son of Arathorn",
         username: "aragorn",
-        password: "hello"
-      }
-    })
+        password: "hello",
+      },
+    });
 
     await request.post("api/users", {
-      data:{
+      data: {
         name: "Legolas",
         username: "legolas",
-        password: "ethelum"
-      }
-    })
+        password: "ethelum",
+      },
+    });
 
-    await page.goto("/")
+    await page.goto("/");
+  });
 
-  })
-
-  test("Homepage is loaded", async({page})=>{
-    const locator = await page.getByText("blogs list")
-    await expect(locator).toBeVisible()
-  })
+  test("Homepage is loaded", async ({ page }) => {
+    const locator = await page.getByText("blogs list");
+    await expect(locator).toBeVisible();
+  });
 
   describe("Loging Form testing", () => {
+    test("Login form is shown", async ({ page }) => {
+      const loginButton = page.getByRole("button", { name: "login" });
+      const usernameInput = page.getByTestId("username");
+      const passwordInput = page.getByTestId("password");
 
-    test("Login form is shown", async({page}) => {
+      await expect(loginButton).toBeVisible();
+      await expect(usernameInput).toBeVisible();
+      await expect(passwordInput).toBeVisible();
+    });
 
-      const loginButton = page.getByRole("button",  {name: "login"})
-      const usernameInput = page.getByTestId("username")
-      const passwordInput = page.getByTestId("password")
-
-      await expect(loginButton).toBeVisible()
-      await expect(usernameInput).toBeVisible()
-      await expect(passwordInput).toBeVisible()
-      
-    })
-
-    test("succeeds with correct credentiaals", async({page}) =>{
+    test("succeeds with correct credentiaals", async ({ page }) => {
       // const loginButton = page.getByRole("button", {name: "login"})
       // const usernameInput = page.getByTestId("username")
       // const passwordInput = page.getByTestId("password")
@@ -53,122 +48,124 @@ describe("Blog app",() => {
       // await usernameInput.fill("legolas")
       // await  passwordInput.fill("ethelum")
       // await loginButton.click()
-      await loginWith(page, "legolas", "ethelum")
+      await loginWith(page, "legolas", "ethelum");
 
-      await(expect(page.getByText("logged in as legolas"))).toBeVisible()
-      
-    })
+      await expect(page.getByText("logged in as legolas")).toBeVisible();
+    });
 
-    test("fails with wrong credentials", async({page}) => {
-      const loginButton = page.getByRole("button", {name: "login"})
-      const usernameInput = page.getByTestId("username")
-      const usernamePassword = page.getByTestId("password")
+    test("fails with wrong credentials", async ({ page }) => {
+      const loginButton = page.getByRole("button", { name: "login" });
+      const usernameInput = page.getByTestId("username");
+      const usernamePassword = page.getByTestId("password");
 
-      await usernameInput.fill("legolas")
-      await usernamePassword.fill("edro")
-      await loginButton.click()
+      await usernameInput.fill("legolas");
+      await usernamePassword.fill("edro");
+      await loginButton.click();
 
-      await(expect(page.getByText("wrong username or password"))).toBeVisible()
+      await expect(page.getByText("wrong username or password")).toBeVisible();
+    });
+  });
+  describe("when logged in", () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, "aragorn", "hello");
+    });
 
-    })
+    test("a new blog can be created", async ({ page }) => {
+      const addBlogButton = page.getByRole("button", { name: "Add new Blog" });
+      await addBlogButton.click();
 
-  })
-  describe( "when logged in", ()=> {
-    beforeEach(async ({page}) => {
-      await loginWith(page, "aragorn", "hello")
-    })
+      const addNewButton = page.getByRole("button", { name: "Add new" });
+      const inputBlogTitle = page.getByTestId("blogTitle");
+      const inputBlogAuthor = page.getByTestId("blogAuthor");
+      const inputBlogUrl = page.getByTestId("blogUrl");
 
-    test("a new blog can be created", async({page}) => {
-      const addBlogButton = page.getByRole("button", {name: "Add new Blog"})
-      await addBlogButton.click()
-      
-      const addNewButton = page.getByRole("button", {name:"Add new"})
-      const inputBlogTitle = page.getByTestId("blogTitle")
-      const inputBlogAuthor = page.getByTestId("blogAuthor")
-      const inputBlogUrl = page.getByTestId("blogUrl")
+      await inputBlogTitle.fill("Adventures in the Middle Earth");
+      await inputBlogAuthor.fill("Gimli");
+      await inputBlogUrl.fill("google.com");
 
+      await addNewButton.click();
 
-      await inputBlogTitle.fill("Adventures in the Middle Earth")
-      await inputBlogAuthor.fill("Gimli")
-      await inputBlogUrl.fill("google.com")
+      await expect(addNewButton).toBeVisible();
+      const locator = page.getByText("Adventures in the Middle Earth").first();
+      await expect(locator).toBeVisible();
+    });
+  });
 
-      await addNewButton.click()
+  describe("other user logged in", () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, "aragorn", "hello");
+      await createDummyEntry(page, [
+        {
+          blogTitle: "The Hobbit",
+          blogAuthor: "Bilbo",
+          blogUrl: "theshire.com",
+        },
+        { blogTitle: "The Shire", blogAuthor: "Sam", blogUrl: "theshire.com" },
+      ]);
+    });
 
-      await expect(addNewButton).toBeVisible()
-      const locator = page.getByText("Adventures in the Middle Earth").first()
-      await expect(locator).toBeVisible()
+    test("user cannot delete entry", async ({ page }) => {
+      await logOutFrom(page);
+      await loginWith(page, "legolas", "ethelum");
 
-    })
+      await expect(page.getByText("logged in as legolas")).toBeVisible();
+      await expect(page.getByText("The Hobbit")).toBeVisible();
 
-  })
+      let viewEntryButton = page.getByRole("button", { name: "view" }).first();
+      let removeEntryButton = page
+        .getByRole("button", { name: "- remove -" })
+        .first();
 
-   describe("other user logged in", () => {
-
-    beforeEach(async ({page}) => {
-      await loginWith(page, "aragorn", "hello")
-      await createDummyEntry(page, [{blogTitle: "The Hobbit", blogAuthor: "Bilbo", blogUrl: "theshire.com"}, {blogTitle: "The Shire", blogAuthor: "Sam", blogUrl: "theshire.com"}])
-    })
-
-    test("user cannot delete entry", async({page}) => {
-      await logOutFrom(page)
-      await loginWith(page, "legolas", "ethelum")
-
-      await(expect(page.getByText("logged in as legolas"))).toBeVisible()
-      await expect(page.getByText("The Hobbit")).toBeVisible()
-
-      let viewEntryButton = page.getByRole("button", {name:"view"}).first()
-      let removeEntryButton = page.getByRole("button", {name:"- remove -"}).first()
-      
-      await viewEntryButton.click() 
-      await removeEntryButton.click()
-      await expect(page.getByText("The Hobbit")).toBeVisible()
+      await viewEntryButton.click();
+      await removeEntryButton.click();
+      await expect(page.getByText("The Hobbit")).toBeVisible();
 
       // DElete entry
-      await logOutFrom(page)
-      await loginWith(page, "aragorn", "hello")
+      await logOutFrom(page);
+      await loginWith(page, "aragorn", "hello");
 
-      viewEntryButton = page.getByRole("button", {name:"view"}).first()
-      removeEntryButton = page.getByRole("button", {name:"- remove -"}).first()
+      viewEntryButton = page.getByRole("button", { name: "view" }).first();
+      removeEntryButton = page
+        .getByRole("button", { name: "- remove -" })
+        .first();
 
-      await viewEntryButton.click() 
-      await removeEntryButton.click()
+      await viewEntryButton.click();
+      await removeEntryButton.click();
 
-      const deletedBlog = page.getByText("The Hobbit")
-      await expect(page.getByText("The Shire")).toBeVisible()
-      
-      await expect(deletedBlog).toBeHidden()
+      const deletedBlog = page.getByText("The Hobbit");
+      await expect(page.getByText("The Shire")).toBeVisible();
 
+      await expect(deletedBlog).toBeHidden();
+    });
+  });
 
-    })
+  describe("order", () => {
+    beforeEach(async ({ page }) => {
+      await loginWith(page, "aragorn", "hello");
+      await createDummyEntry(page, [
+        {
+          blogTitle: "The Hobbit",
+          blogAuthor: "Bilbo",
+          blogUrl: "theshire.com",
+        },
+        { blogTitle: "The Shire", blogAuthor: "Sam", blogUrl: "theshire.com" },
+      ]);
+    });
 
-   })
+    test("check blogs order", async ({ page }) => {
+      let viewEntryButton = page.getByRole("button", { name: "view" }).first();
+      await viewEntryButton.click();
+      await page.reload();
 
-   describe("order", () => {
-
-    beforeEach(async ({page}) => {
-      await loginWith(page, "aragorn", "hello")
-      await createDummyEntry(page, [{blogTitle: "The Hobbit", blogAuthor: "Bilbo", blogUrl: "theshire.com"}, {blogTitle: "The Shire", blogAuthor: "Sam", blogUrl: "theshire.com"}])
-    })
-
-    test("check blogs order", async({page}) => {
-      
-
-      let viewEntryButton = page.getByRole("button", {name:"view"}).first()
-      await viewEntryButton.click() 
-      await page.reload()
-
-      const likeButtons = page.locator('.entryLikes').all()
+      const likeButtons = page.locator(".entryLikes").all();
 
       for (let button of likeButtons) {
-        button.click()
+        button.click();
       }
-      
 
-      const checkText = page.getByText("The Hobbit")
-      
-      await expect(checkText).toBeVisible()
+      const checkText = page.getByText("The Hobbit");
 
-    })
-   })
-
-})
+      await expect(checkText).toBeVisible();
+    });
+  });
+});
--- part5/bloglist-testing/tests/helper.js
+++ part5/bloglist-testing/tests/helper.js
@@ -1,42 +1,38 @@
 const loginWith = async (page, username, password) => {
+  const loginButton = page.getByRole("button", { name: "login" });
+  const usernameInput = page.getByTestId("username");
+  const passwordInput = page.getByTestId("password");
 
-      const loginButton = page.getByRole("button", {name: "login"})
-      const usernameInput = page.getByTestId("username")
-      const passwordInput = page.getByTestId("password")
+  await usernameInput.fill(username);
+  await passwordInput.fill(password);
 
-      await usernameInput.fill(username)
-      await  passwordInput.fill(password)
-
-      await loginButton.click()
-}
+  await loginButton.click();
+};
 const logOutFrom = async (page) => {
-      const logoutButton = page.getByRole("button", {name:"logout"})
-      await logoutButton.click()
-}
+  const logoutButton = page.getByRole("button", { name: "logout" });
+  await logoutButton.click();
+};
 /**
- * 
- * @param {*} page 
+ *
+ * @param {*} page
  * @param {Array} data Array of Objects
  */
 const createDummyEntry = async (page, data) => {
+  const addBlogButton = page.getByRole("button", { name: "Add new Blog" });
+  await addBlogButton.click();
 
-      const addBlogButton = page.getByRole("button", {name: "Add new Blog"})
-      await addBlogButton.click()
-      
-      for (let i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) {
+    const addNewButton = page.getByRole("button", { name: "Add new" });
+    const inputBlogTitle = page.getByTestId("blogTitle");
+    const inputBlogAuthor = page.getByTestId("blogAuthor");
+    const inputBlogUrl = page.getByTestId("blogUrl");
 
-      const addNewButton = page.getByRole("button", {name:"Add new"})
-      const inputBlogTitle = page.getByTestId("blogTitle")
-      const inputBlogAuthor = page.getByTestId("blogAuthor")
-      const inputBlogUrl = page.getByTestId("blogUrl")
+    await inputBlogTitle.fill(data[i].blogTitle);
+    await inputBlogAuthor.fill(data[i].blogAuthor);
+    await inputBlogUrl.fill(data[i].blogUrl);
 
+    await addNewButton.click();
+  }
+};
 
-      await inputBlogTitle.fill(data[i].blogTitle)
-      await inputBlogAuthor.fill(data[i].blogAuthor)
-      await inputBlogUrl.fill(data[i].blogUrl)
-
-      await addNewButton.click()
-      }
-}
-
-export {loginWith, createDummyEntry, logOutFrom}
+export { loginWith, createDummyEntry, logOutFrom };
