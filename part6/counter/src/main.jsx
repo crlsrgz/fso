6e684d986512de9d2892bab4007b7e7e2fbd9e68
crlsrgz <course@carlosfx.com>
2025-02-25 13:12:21 +0100

    Part 6a: App moved to one file inside store

--- part6/counter/src/main.jsx
+++ part6/counter/src/main.jsx
@@ -1,4 +1,62 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import "./App.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+import { createStore } from "redux";
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    case "ZERO":
+      return 0;
+    default: // if none of the above matches, code comes here
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+store.subscribe(() => {
+  const storeNow = store.getState();
+  console.log("store now", storeNow);
+});
+
+// store.dispatch({ type: "INCREMENT" });
+// store.dispatch({ type: "INCREMENT" });
+// store.dispatch({ type: "INCREMENT" });
+// store.dispatch({ type: "ZERO" });
+// store.dispatch({ type: "DECREMENT" });
+
+// console.log(store.getState());
+
+function App() {
+  return (
+    <>
+      <div>Cont: {store.getState()}</div>
+
+      <div className="card">
+        <button onClick={(e) => store.dispatch({ type: "INCREMENT" })}>
+          plus
+        </button>
+        <button onClick={(e) => store.dispatch({ type: "DECREMENT" })}>
+          minus
+        </button>
+        <button onClick={(e) => store.dispatch({ type: "ZERO" })}>zero</button>
+      </div>
+    </>
+  );
+}
+
+// store.subscribe();
+
+export default App;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const renderApp = () => {
+  root.render(<App />);
+};
+
+renderApp();
+store.subscribe(renderApp);
